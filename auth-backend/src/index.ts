2ec809e21b7a66f207112bb11ee236b11127a290
Rajat Pathak <rajatpathak786@gmail.com>
2024-02-24 04:26:41 +0530

    Added logic to make connection with mongodb

--- auth-backend/src/index.ts
+++ auth-backend/src/index.ts
@@ -2,14 +2,24 @@ import express from "express";
 import { json } from "body-parser";
 import router from "./routes";
 import { errorHandler } from "./middleware/error-handler";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(json());
 
-app.listen(3000, () => {
-  console.log(`Running on port 3000`);
-});
-
 app.use(`/api`, router);
 
 app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth-db");    
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log(`Running on port 3000`);
+  });
+};
+
+start();
