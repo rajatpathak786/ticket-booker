87286766e44d7c7c1dcb2afb528a154a8448ec41
Rajat Pathak <rajatpathak786@gmail.com>
2024-02-25 02:58:09 +0530

    Updated logic for hashing password

--- auth-backend/src/utils/hashing.ts
+++ auth-backend/src/utils/hashing.ts
@@ -1,18 +1,40 @@
-import bcrypt from "bcrypt";
-const saltRounds = 10;
+import { scrypt, randomBytes } from "crypto";
+import { promisify } from "util";
+
+const scryptAsync = promisify(scrypt);
 
 export const generatePasswordHash = async (
   password: string
 ): Promise<string> => {
-  console.log(`----------------`);
-  const salt = await bcrypt.genSalt(saltRounds);
-  console.log(salt);
-  return await bcrypt.hash(password, salt);
+  const salt = randomBytes(8).toString("hex");
+  const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+
+  return `${buf.toString("hex")}.${salt}`;
 };
 
 export const comparePasswordHash = async (
-  password: string,
-  hash: string
+  storedPassword: string,
+  suppliedPassword: string
 ): Promise<boolean> => {
-  return await bcrypt.compare(password, hash);
+  const [hashedPassword, salt] = storedPassword.split(".");
+  const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+  return buf.toString("hex") === hashedPassword;
 };
+
+// import bcrypt from "bcrypt";
+// const saltRounds = 10;
+
+// export const generatePasswordHash = async (
+//   password: string
+// ): Promise<string> => {
+//   const salt = await bcrypt.genSalt(saltRounds);
+//   return await bcrypt.hash(password, salt);
+// };
+
+// export const comparePasswordHash = async (
+//   password: string,
+//   hash: string
+// ): Promise<boolean> => {
+//   return await bcrypt.compare(password, hash);
+// };
